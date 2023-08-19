213b1636a0e2d14bec67c732aa4529badd1d4011
Elshaman <cbuitragoo.2023@gmail.com>
2023-08-19 18:28:41 -0500

    pejemplo básico

---
+++ src/main/java/org/cbo/jdbc/EjemploJDBC.java
@@ -0,0 +1,32 @@
+package org.cbo.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class EjemploJDBC {
+
+    public static void main(String[] args) {
+            String url = "jdbc:mysql://localhost:3310/bansaa";
+            String username = "root";
+            String password = "admin";
+            try {
+                Connection conn = DriverManager.getConnection(url, username, password);
+                Statement statement =  conn.createStatement();
+                ResultSet resultSet = statement.executeQuery("SELECT * from competencias");
+                while(resultSet.next()){
+                    System.out.println(resultSet.getString("nombre"));
+                }
+                resultSet.close();
+                statement.close();
+                conn.close();
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());    
+            }
+    }
+
+   
+    
+}
