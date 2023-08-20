ca4084eeece75497bd22c2108f89487f8d792496
Elshaman <cbuitragoo.2023@gmail.com>
2023-08-20 07:41:02 -0500

    Singleton implementado

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 JDBC Ejemplos
 
 1) Ejemplo básico con Query SQL
+2) Ejemplo con try - catch - finally
+3) Try con recurso
+4)
--- src/main/java/org/cbo/jdbc/EjemploJDBC.java
+++ src/main/java/org/cbo/jdbc/EjemploJDBC.java
@@ -6,13 +6,13 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import org.cbo.jdbc.util.ConexionBaseDatos;
+
 public class EjemploJDBC {
 
     public static void main(String[] args) {
-            String url = "jdbc:mysql://localhost:3310/bansaa";
-            String username = "root";
-            String password = "admin";
-            try (Connection conn = DriverManager.getConnection(url, username, password);
+          
+            try (Connection conn = ConexionBaseDatos.obtenerInstancia();
                 Statement statement =  conn.createStatement();
                 ResultSet resultSet = statement.executeQuery("SELECT * from competencias")){
                 
